7d044d60f3bd87bd38736f0fef193cf1aef0e2d4
seochanhuck <seochanhucK@gmail.com>
2026-03-10 10:05:52 +0900

    [db] task, book_progress 추가

---
+++ src/main/java/com/team/student_calendar/entity/TaskEntity.java
@@ -0,0 +1,41 @@
+package com.team.student_calendar.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "task", schema = "student_calendar")
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            nullable = false,
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    private StudentEntity studentEntity;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "due_at")
+    private LocalDateTime dueAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP(6)")
+    @Column(name = "registered_at", nullable = false)
+    private LocalDateTime registeredAt;
+}
