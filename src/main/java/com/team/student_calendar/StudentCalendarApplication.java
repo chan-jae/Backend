3d1c15bee10d144ee8ad98e31557c6fbdc19324f
seochanhuck <seochanhucK@gmail.com>
2026-04-14 10:17:37 +0900

    [chore] 타임존을 시작할 때 Asia/Seoul로 설정

--- src/main/java/com/team/student_calendar/StudentCalendarApplication.java
+++ src/main/java/com/team/student_calendar/StudentCalendarApplication.java
@@ -1,10 +1,19 @@
 package com.team.student_calendar;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StudentCalendarApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentCalendarApplication.class, args);
