6d33b94d38549462a09f3ee620ee72fbf7d120c2
seochanhuck <seochanhucK@gmail.com>
2026-04-14 10:33:50 +0900

    [chore] 타임존 설정 run 이전으로 변경

--- src/main/java/com/team/student_calendar/StudentCalendarApplication.java
+++ src/main/java/com/team/student_calendar/StudentCalendarApplication.java
@@ -1,6 +1,5 @@
 package com.team.student_calendar;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,13 +8,8 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class StudentCalendarApplication {
 
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(StudentCalendarApplication.class, args);
 	}
 
